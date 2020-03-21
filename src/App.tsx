24eb3470b6e7c363ab6d3137aaff6adab155d340
Majewski <mmajews2@MGC12YJ052JGH5.fbpld77.ford.com>
2020-03-21 18:32:26 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import data from "./categories.json";
 
+const letters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "R",
+  "S",
+  "T",
+  "W"
+];
 function App() {
+  const [gameNum, setGameNum] = useState<number>(0);
+  const [seconds, setSeconds] = useState(30);
+  const [start, setStart] = useState(false);
+  const [letter, setLetter] = useState("");
+
+  useEffect(() => {
+    if (!start) {
+      return;
+    }
+    if (seconds === 0) setStart(false);
+    const interval = setInterval(() => {
+      setSeconds(seconds - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [start, seconds]);
+  const printList = () => {
+    const list = data[gameNum];
+    const index = gameNum;
+    //@ts-ignore
+    return list[index + 1].map((cat, index) => {
+      return (
+        <li className="list-item" key={index}>
+          {cat}
+        </li>
+      );
+    });
+  };
+
+  const getLetter = () => {
+    const index = Math.floor(Math.random() * (20 - 1)) + 1;
+    console.log("index: ", index);
+    console.log("letter:", letters[index]);
+    setLetter(letters[index]);
+  };
+  const nextGame = () => {
+    setStart(true);
+    setSeconds(30);
+    getLetter();
+    if (gameNum === 10) {
+      setGameNum(0);
+    } else {
+      setGameNum(gameNum + 1);
+    }
+  };
+  const showGame = () => {
+    if (start) {
+      return (
+        <div className="list">
+          <div className="left first">
+            <h1>List #{gameNum + 1}</h1>
+            <div>
+              <ul>{printList()}</ul>
+            </div>
+          </div>
+          <div className="left">
+            <h2>Letter</h2>
+            <div className="letter">{letter}</div>
+          </div>
+        </div>
+      );
+    }
+  };
+  const printSeconds = () => {
+    if (seconds === 0) return <h1>Times Up!</h1>;
+    else return <h1> {seconds} </h1>;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Time Remaining</h2>
+      <p>{printSeconds()}</p>
+      <button className="start-button" onClick={nextGame}>
+        Start Clock
+      </button>
+      {showGame()}
     </div>
   );
 }
