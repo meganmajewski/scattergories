ec23b8e7058b043ba90f635eb1fab5650ea25695
megan majewski <mmajews2@ford.com>
2021-02-25 13:10:21 -0500

    remove port for remote server

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
   const [gameOver, setGameOver] = useState<boolean>(true);
   const [letter, setLetter] = useState<string>("");
   const [response, setResponse] = useState("");
-  const ENDPOINT = "https://scattegories.herokuapp.com:4001";
+  const ENDPOINT = "https://scattegories.herokuapp.com";
   useEffect(() => {
     const socket = io(ENDPOINT);
     //@ts-ignore
