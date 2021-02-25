9067218d8e1fcf82d47f5c3763da33b9a21ceca4
megan majewski <mmajews2@ford.com>
2021-02-25 11:43:01 -0500

    remove express.use

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-express.use(express.static(path.join(__dirname, "build/")))
+// express.use(express.static(path.join(__dirname, "build/")))
 router.get("/", (_, res) =>
   res.sendFile(path.join(__dirname, "build", "index.html"))
 )
